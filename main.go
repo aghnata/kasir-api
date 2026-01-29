1802ad6323de94592963ff35ab2cf4b82a40b489
Aghnat Atqiya <aghnatqiya@gmail.com>
2026-01-29 07:15:31 +0700

    remove unnecessary comment

--- main.go
+++ main.go
@@ -49,39 +49,6 @@ func main() {
 	//setup routes
 	http.HandleFunc("/api/products", productHandler.HandleProducts)
 
-	//GET localhost:8080/api/produk/{id}
-	//PUT localhost:8080/api/produk/{id}
-	//DELETE localhost:8080/api/produk/{id}
-	// http.HandleFunc("/api/produk/", func(w http.ResponseWriter, r *http.Request) {
-	// 	if r.Method == "GET" {
-	// 		getProdukByID(w, r)
-	// 	} else if r.Method == "PUT" {
-	// 		updateProdukByID(w, r)
-	// 	} else if r.Method == "DELETE" {
-	// 		deleteProdukByID(w, r)
-	// 	}
-	// })
-
-	//GET localhost:8080/api/produk
-	//POST localhost:8080/api/produk
-	// http.HandleFunc("/api/produk", func(w http.ResponseWriter, r *http.Request) {
-	// 	if r.Method == "GET" {
-	// 		w.Header().Set("Content-Type", "application/json")
-	// 		json.NewEncoder(w).Encode(produk)
-	// 	} else if r.Method == "POST" {
-	// 		var produkBaru models.Produk
-	// 		err := json.NewDecoder(r.Body).Decode(&produkBaru)
-	// 		if err != nil {
-	// 			http.Error(w, err.Error(), http.StatusBadRequest)
-	// 			return
-	// 		}
-	// 		produkBaru.ID = len(produk) + 1
-	// 		produk = append(produk, produkBaru)
-	// 		w.Header().Set("Content-Type", "application/json")
-	// 		json.NewEncoder(w).Encode(produkBaru)
-	// 	}
-	// })
-
 	// /health endpoint
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
