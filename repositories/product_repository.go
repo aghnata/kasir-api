d77a9bbf16959846b257354280b011622af961e4
Aghnat Atqiya <aghnatqiya@gmail.com>
2026-01-31 00:28:54 +0700

    add relation category and product

--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -15,7 +15,7 @@ func NewProductRepository(db *sql.DB) *ProductRepository {
 }
 
 func (repo *ProductRepository) GetAll() ([]models.Product, error) {
-	query := "SELECT id, name, price, stock FROM products"
+	query := "SELECT p.id, p.name, p.price, p.stock, c.name as category FROM products p left join categories c ON p.category_id = c.id"
 	rows, err := repo.db.Query(query)
 	if err != nil {
 		return nil, err
@@ -25,7 +25,7 @@ func (repo *ProductRepository) GetAll() ([]models.Product, error) {
 	products := make([]models.Product, 0)
 	for rows.Next() {
 		var product models.Product
-		err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.Stock)
+		err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.Stock, &product.Category)
 		if err != nil {
 			return nil, err
 		}
@@ -45,8 +45,8 @@ func (repo *ProductRepository) Create(product *models.Product) error {
 	// e.g., check if name is not empty, price is positive, etc.
 
 	// Use parameterized query to prevent SQL injection
-	query := "INSERT INTO products (name, price, stock) VALUES ($1, $2, $3) RETURNING id"
-	err := repo.db.QueryRow(query, product.Name, product.Price, product.Stock).Scan(&product.ID)
+	query := "INSERT INTO products (name, price, stock, category_id) VALUES ($1, $2, $3, (SELECT id FROM categories WHERE name = $4)) RETURNING id"
+	err := repo.db.QueryRow(query, product.Name, product.Price, product.Stock, product.Category).Scan(&product.ID)
 	if err != nil {
 		return err
 	}
@@ -55,10 +55,10 @@ func (repo *ProductRepository) Create(product *models.Product) error {
 }
 
 func (repo *ProductRepository) GetByID(id int) (*models.Product, error) {
-	query := "SELECT id, name, price, stock FROM products WHERE id = $1"
+	query := "SELECT p.id, p.name, p.price, p.stock, c.name as category FROM products p left join categories c ON p.category_id = c.id WHERE p.id = $1"
 	row := repo.db.QueryRow(query, id)
 	var product models.Product
-	err := row.Scan(&product.ID, &product.Name, &product.Price, &product.Stock)
+	err := row.Scan(&product.ID, &product.Name, &product.Price, &product.Stock, &product.Category)
 
 	if err == sql.ErrNoRows {
 		return nil, errors.New("produk tidak ditemukan")
@@ -72,8 +72,8 @@ func (repo *ProductRepository) GetByID(id int) (*models.Product, error) {
 
 func (repo *ProductRepository) Update(data *models.Product) error {
 	// Implementation for updating a product
-	query := "UPDATE products SET name = $1, price = $2, stock = $3 WHERE id = $4"
-	result, err := repo.db.Exec(query, data.Name, data.Price, data.Stock, data.ID)
+	query := "UPDATE products SET name = $1, price = $2, stock = $3, category_id = (SELECT id FROM categories WHERE name = $4) WHERE id = $5"
+	result, err := repo.db.Exec(query, data.Name, data.Price, data.Stock, data.Category, data.ID)
 	if err != nil {
 		return err
 	}
